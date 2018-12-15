12ae70256d26d2cfba078d3144dc6f9b6f8cacb1
dorisgjata <dorisgjata@oakland.edu>
2018-12-14 19:21:21 -0500

    initial appbar

---
+++ src/TopAppbar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { withStyles } from '@material-ui/core/styles';
+
+class TopAppBar extends React.Component {
+    render(){
+        const {classes}=this.props
+        return(
+            <div className={classes.root}>
+            <AppBar className={classes.toolbar} position="static">
+            <Toolbar variant="dense">
+            <IconButton  color="inherit" aria-label="menu"><MenuIcon/></IconButton>
+            <Typography color="inherit">
+                curlyD
+            </Typography>
+            </Toolbar>
+            </AppBar>
+
+            </div>
+        )
+    }
+
+}
+
+TopAppBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    toolbar:{
+        backgroundColor: "#5C6BC0"
+    },
+    
+  };
+  export default withStyles(styles)(TopAppBar);
