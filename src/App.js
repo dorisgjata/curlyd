48f5c3e4e470512da6d5425e83c477f6d56ee353
dorisgjata <dorisgjata@oakland.edu>
2018-12-28 19:46:48 -0500

    Typing animations

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import TopAppbar from './components/TopAppbar';
+import { Typography } from '@material-ui/core';
+import Typing from 'react-typing-animation';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-         <TopAppbar/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<TopAppbar />
+				<Typing speed={70}>
+					<div className="typing">Hello there! I'm curlyD.</div>
+				</Typing>
+			</div>
+		);
+	}
 }
 
 export default App;
